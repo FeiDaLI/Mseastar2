839eb2e3397f67c99f8a7c831adee7458c7aef05
FeiDa <1607741781@qq.com>
2025-05-04 12:43:08 +0800

    ccghh

--- build/include/future/future_all11.d
+++
@@ -1,8 +0,0 @@
-build/include/future/future_all11.o: include/future/future_all11.cc \
- include/future/future_all11.hh include/future/../task/task.hh \
- include/future/../util/shared_ptr.hh include/future/do_with.hh \
- include/future/../resource/resource.hh include/future/../util/align.hh \
- include/future/../util/bool_class.hh include/future/../util/spinlock.hh \
- include/future/../util/unaligned.hh include/future/../util/bitops.hh \
- include/future/../fd/posix.hh include/future/../util/backtrace.hh \
- include/future/future_all11.tpp
--- build/include/resource/resource.d
+++
@@ -1,3 +0,0 @@
-build/include/resource/resource.o: include/resource/resource.cc \
- include/resource/resource.hh include/resource/../util/align.hh \
- include/resource/../util/defer.hh
